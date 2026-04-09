283254355ce92d02c92a76265f6d5fbbc603345c
iamgnanesh108 <gnaneshmummadi108@gmail.com>
2026-04-09 21:38:08 +0530

    Update setupdc.sh

--- setupdc.sh
+++ setupdc.sh
@@ -19,5 +19,7 @@ sudo groupadd docker || true
 sudo usermod -a -G docker ec2-user
 sudo chmod 666 /var/run/docker.sock
 
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 docker-compose up --scale myapp=2 -d
