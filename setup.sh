db50bdb97ca93842f51527f040f864b2647ca6c4
iamgnanesh108 <gnaneshmummadi108@gmail.com>
2026-04-09 20:57:03 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+# Switch to root
+#sudo su -
 
+#---Install Git ---
+yum install git -y
+git clone https://github.com/iamgnanesh108/Playwright.git
+cd Playwright
+
+
+
+# --- Install Docker ---
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+
+# Add ec2-user to docker group
+sudo groupadd docker || true
+sudo usermod -a -G docker ec2-user
+sudo chmod 666 /var/run/docker.sock
+
+#-------Build image------
+docker build -t pwimage .
+
+#------run image----
+docker run pwimage
+
+docker rm -f pwimage
